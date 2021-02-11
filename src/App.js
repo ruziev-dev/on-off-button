03a6a0d675fb49584013055b0454dc57ee2ce3d2
Timur Ruziev <ruziev.dev@gmail.com>
2021-02-11 20:03:44 +0300

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { useDispatch, useSelector } from 'react-redux'
 const App = () => {
   const dispatch = useDispatch()
   const buttonState = useSelector(state => state.buttonState)
-  
+
   const onButtonClick = () => dispatch({ type: 'CHANGE_BUTTON_STATE' })
 
   return (
     <div className="wrapper">
       <button className="circularButton" onClick={onButtonClick}>
         {
-        buttonState ? "ON" : "OFF"
+          buttonState ? "ON" : "OFF"
         }
       </button>
     </div>
